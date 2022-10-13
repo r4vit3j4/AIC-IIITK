6e8649b3347489ac547628436b40febe2c5b9fff
Srujan <srujan@Srujans-MacBook-Air.local>
2022-10-13 12:57:08 +0530

    modified footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,41 @@
+import { Flex, Heading, Image, Text } from "@chakra-ui/react";
 import React from "react";
+import { Box } from "@chakra-ui/react";
+import {AiFillFacebook} from  "react-icons/ai";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  const property = {
+    imageUrl: "/assets/logos/nlogo.png",
+  };
+
+  return (
+    <div>
+      <Box bg="black" w="100%" p={4} color="white">
+        <Box align="center">
+          <Image
+            boxSize="100px"
+            objectFit="contain"
+            src={property.imageUrl}
+            alt="image"
+          />
+          <Flex w="100%" justify="center" my="10">
+          <AiFillFacebook size={35} color="white"/>
+          <AiFillFacebook size={35} color="white"/>
+          <AiFillFacebook size={35} color="white"/>
+          <AiFillFacebook size={35} color="white"/>
+          <AiFillFacebook size={35} color="white"/>
+
+
+          </Flex>
+          
+          <Text color="white">
+            COPYRIGHT © 2019-{new Date().getFullYear()}. ALL RIGHTS RESERVED. DESIGNED BY IOT CLOUD
+            RESEARCH TEAM OF IIIT KOTTAYAM. Visitors{" "}
+          </Text>
+        </Box>
+      </Box>
+    </div>
+  );
 };
 
 export default Footer;
